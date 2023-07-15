0e5a92a531209b0f45e83cb82d817b58a60dd13b
Janybek <best.joni.547545@gmail.com>
2023-07-15 10:16:30 +0600

    add env

--- src/services/Server.js
+++ src/services/Server.js
@@ -1,3 +1,4 @@
-const BASE_URL = 'https://sneakers-bj06.onrender.com/'
+// const DEV_URL = 'http://localhost:8083/'
+const BASE_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:8083/'
 
 export default BASE_URL
